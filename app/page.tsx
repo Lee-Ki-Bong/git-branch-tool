ce0ce33898b64b89777dbeb2bec3ebd1be512b84
dhkehd2 <dhkehd2@gmail.com>
2024-12-18 13:17:59 +0900

    💄 그라데이션 배경

--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,26 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <section className="container flex min-h-screen flex-wrap content-evenly items-center justify-center gap-8 px-8">
-      <div className="flex-1">
+    <section className="container flex flex-col gap-8 py-20">
+      <div className="glass p-8">
         <h1 className="text--neon--normal h1">404</h1>
         <h2 className="text--neon--normal h3">Page Not Found</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim
+          exercitationem optio, provident ab earum labore tempora laborum
+          officia laboriosam ratione, voluptatem praesentium saepe et? Placeat
+          eaque aliquid sapiente iusto cumque!
+        </p>
       </div>
-      <div className="flex-1">
-        <h1 className="text--neon--white h1">404</h1>
-        <h2 className="text--neon--white h3">Page Not Found</h2>
-      </div>
-      <div className="flex-1">
-        <h1 className="text--neon--green h1">404</h1>
-        <h2 className="text--neon--green h3">Page Not Found</h2>
-      </div>
-      <div className="flex-1">
-        <h1 className="text--neon--red h1">404</h1>
-        <h2 className="text--neon--red h3">Page Not Found</h2>
-      </div>
-      <div className="flex-1">
-        <h1 className="text--neon--orange h1">404</h1>
-        <h2 className="text--neon--orange h3">Page Not Found</h2>
-      </div>
-      <div className="border--neon--purple flex-1 p-8">
+      <div className="glass flex-auto p-8">
         <h1 className="text--neon--purple h1">404</h1>
         <h2 className="text--neon--purple h3">Page Not Found</h2>
-      </div>
-      <div className="flex-1">
-        <h1 className="text--neon--blue h1">404</h1>
-        <h2 className="text--neon--blue h3">Page Not Found</h2>
-      </div>
-      <div className="flex-1">
-        <h1 className="h1">404</h1>
-        <h2 className="h3">Page Not Found</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+          perspiciatis nihil autem ullam qui odit sequi sed quia, unde fugiat
+          rem impedit obcaecati voluptates, iusto, iste facilis! Eum, vitae
+          quasi!
+        </p>
       </div>
     </section>
   )
