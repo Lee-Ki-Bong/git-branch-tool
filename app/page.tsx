2d93effe2c147794dd4e735b2ea0823c6113fa72
dhkehd2 <dhkehd2@gmail.com>
2024-12-18 14:00:28 +0900

    💄 공통 여백

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-import SVGUser from "./components/svgs/SvgUser"
-
 export default function Home() {
   return (
-    <section className="container flex flex-col gap-8 py-20">
-      <div className="glass p-8">
-        <h1 className="text--neon--white h1">404</h1>
-        <h2 className="text--neon--white h3">Page Not Found</h2>
-      </div>
-      <div className="glass flex-auto p-8">
-        <h1 className="text--neon--purple h1">404</h1>
-        <h2 className="text--neon--purple h3">Page Not Found</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-          perspiciatis nihil autem ullam qui odit sequi sed quia, unde fugiat
-          rem impedit obcaecati voluptates, iusto, iste facilis! Eum, vitae
-          quasi!
-        </p>
-      </div>
-      <div className="glass p-8">
-        <h1 className="text--neon--orange h1">404</h1>
-        <h2 className="text--neon--orange h3">
-          Page Not Found{" "}
-          <SVGUser width="20" height="20" className="text-neonOrange" />
-        </h2>
-      </div>
+    <section className="root--container container flex flex-col gap-8">
+      <div className="glass sub-padding"></div>
+      <div className="glass sub-padding flex-auto"></div>
+      <div className="glass sub-padding"></div>
     </section>
   )
 }
