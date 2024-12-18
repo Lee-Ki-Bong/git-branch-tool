8f73debaba84b37b408b8980932a0d70ff28d2ec
dhkehd2 <dhkehd2@gmail.com>
2024-12-18 14:26:56 +0900

    💄 버튼 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
     <section className="root--container container flex flex-col gap-8">
-      <div className="glass sub-padding"></div>
+      <div className="glass sub-padding">
+        <button className="button-neon-normal">Check Out</button>
+      </div>
       <div className="glass sub-padding flex-auto"></div>
-      <div className="glass sub-padding"></div>
+      <div className="glass sub-padding">
+        <button className="button-neon-white">Check Out</button>
+      </div>
     </section>
   )
 }
